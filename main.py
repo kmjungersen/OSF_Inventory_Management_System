618adc01f7c4a6667d35a821021d68d5388e43f9
kmjungersen <kmjungersen@gmail.com>
2014-08-05 09:19:36 -0400

    began architecture for project

---
+++ main.py
@@ -0,0 +1,22 @@
+#imports
+
+import pymongo
+
+
+class Inventory():
+
+    def __init__():
+
+        pass
+
+    def new_record(info):
+
+        pass
+
+    def retrieve_record(info):
+
+        pass
+
+    def modify_record(info):
+
+        pass
