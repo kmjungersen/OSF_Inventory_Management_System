155a4d2d0a1cab16f8c062d233f8d897081ea5ac
kmjungersen <kmjungersen@gmail.com>
2014-12-29 12:28:54 -0600

    starting a new test file

--- test.py
+++ test.py
@@ -1,24 +1,27 @@
 from main import Inventory
 
 
-class TestForProperEntryValidation():
+# class TestForProperEntryValidation():
+#
+#     def __init__(self):
+#
+#         self.test_dict = {
+#             'item_number': 12345,
+#             'barcode_number': 67890,
+#             'description': 'some really cool super awesome thing',
+#             'foo': 'bar',
+#             'room': 'that room across the hall',
+#             'baz': 45,
+#
+#         }
+#
+#         self.inventory = Inventory()
+#
+#     def test_check_for_valid_fields(self):
+#
+#         status, invalid_fields = self.inventory.check_for_valid_fields(self.test_dict)
+#
+#         assert status == False
 
-    def __init__(self):
 
-        self.test_dict = {
-            'item_number': 12345,
-            'barcode_number': 67890,
-            'description': 'some really cool super awesome thing',
-            'foo': 'bar',
-            'room': 'that room across the hall',
-            'baz': 45,
-
-        }
-
-        self.inventory = Inventory()
-
-    def test_check_for_valid_fields(self):
-
-        status, invalid_fields = self.inventory.check_for_valid_fields(self.test_dict)
-
-        assert status == False
+foo = Inventory()
