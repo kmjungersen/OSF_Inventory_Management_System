f61a032fdcd131add5fa157c58235da1a6118332
kmjungersen <kmjungersen@gmail.com>
2015-01-11 21:08:04 -0500

    Completely overhauling the way items are stored and handled

--- items.py
+++ items.py
@@ -2,22 +2,68 @@
 
 """
 
+from database import Database
+
+
 class Item():
 
     def __init__(self):
+        """
 
-        pass
+        :return:
+        """
 
+        self.db = Database()
+        self.is_valid = self.validate()
+
+    def validate(self):
+        """
+
+        :return:
+        """
+        valid = True
+
+        return valid
+
+    def update(self, updated_info):
+        """
+
+        :param updated_info:
+        :return:
+        """
+
+    def checkout(self, barcode):
+        """ This function checks out an item given a barcode number
+
+        :param barcode:
+        :return:
+        """
 
 
 
 class Product():
 
     def __init__(self, record_info):
+        """
 
-        self.info = record_info
+        :param record_info:
+        :return:
+        """
+
+        self.items = []
+        self.info = {}
+
+        self.db = Database()
 
         self.is_valid = self.validate()
+
+    def fetch_data(self):
+        """
+
+        :return:
+        """
+
+        self.info = self.db.find()
 
     def validate(self):
         """ This function validates the data given for the item and will
@@ -38,23 +84,19 @@ class Product():
         :return:
         """
 
+
+
+
+
     #TODO - this should be done using decorators so that every single change
     # is logged
-    def log_change(self, updated_info):
+def log_change(self, updated_info):
         """ This function will log any updates for a record so they can be
         retrieved later if needed.  This allows monitoring of stock levels
         for automatic threshold adjustment.
 
         :return:
         """
-
-    def checkout(self, barcode):
-        """ This function checks out an item given a barcode number
-
-        :param barcode:
-        :return:
-        """
-
 
 
 # class Kit():
