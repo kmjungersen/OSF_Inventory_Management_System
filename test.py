e604ac2f4c976da4ddcc0d0e063feb4f710584c4
kmjungersen <kmjungersen@gmail.com>
2015-02-05 19:33:40 -0500

    added product test class with a few tests

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from items import Item, Product
 
 
 class TestItems:
@@ -49,3 +50,77 @@ class TestItems:
         i = self.setup_item(info)
 
         assert not i.is_valid
+
+
+class TestProducts:
+
+    def setup(self):
+
+        self.test_item_info = {
+            'barcode_id': 12345,
+            'expiration_date': '1/23/15',
+            'room': '5',
+            'unit': '6',
+            'shelf': '2',
+            'date_added': '1/15/15',
+            'most_recently_used': '1/20/15',
+            'purchased_from': 'ACME inc',
+            'cost': '350',
+            'item_number': 78465,
+            'checked_in': True,
+        }
+
+
+        test_items = []
+
+        for x in range[5]:
+
+            self.test_item_info['barcode_id'] = x
+            item = Item(self.test_item_info)
+
+            test_items.append(item)
+
+        self.test_product_info = {
+            'product_number': 1235456,
+            'name': 'foo product',
+            'quantity': 45,
+            'items': test_items,
+            'description': 'foooooo',
+            'notes': 'here are some cool notes',
+            'preferred_supplier': 'ACME',
+            'supplier_notes': 'beep beep',
+            'container_type': 'milk_jug',
+            'average_cost': 4562,
+        }
+
+    def setup_product(self, info=None):
+
+        if not info:
+            info = self.test_product_info
+
+        product = Product(info)
+
+        return product
+
+    def test_product_fields(self):
+
+        p = self.setup_product()
+
+        assert p.info == self.test_product_info
+
+    def test_required_fields(self):
+
+        p = self.setup_product()
+
+        assert p.is_valid
+
+    def test_required_fields_missing(self):
+
+        info = self.test_product_info
+        info.__delitem__('barcode_id')
+
+        p = self.setup_product(info)
+
+        assert not p.is_valid
+
+
