9a40bf029c2ceea70f2b81b637ce87cb693a8ca0
kmjungersen <kmjungersen@gmail.com>
2015-02-20 12:41:01 -0500

    continuing database re-writes

--- database.py
+++ database.py
@@ -6,11 +6,13 @@ from local import *
 
 class Database():
 
-    def __init__(self):
+    def __init__(self, debug=False):
         """ Establishes connection with MongoDB for use
 
         :return:
         """
+
+        self.debugging = debug
 
         client = MongoClient(host=DB_NODE, port=DB_PORT)
 
@@ -36,7 +38,7 @@ class Database():
         """
         record = item.info
 
-        self.__write(record, 'items')
+        self.__write('items', record)
 
     def add_product(self, product):
         """ Adds a single product to the inventory database
@@ -47,16 +49,20 @@ class Database():
 
         record = product.info
 
-        self.__write(record, 'products')
+        self.__write('products', record)
 
-    def update_item(self, item, updated_info):
+    def update_item(self, item):
         """ Updates a single item
 
         :param item:
         :return:
         """
 
+        if item.is_valid:
 
+            new_record = item.info
+
+            self.__update('items', new_record)
 
     def update_product(self, product, updated_info):
         """
@@ -65,20 +71,22 @@ class Database():
         :return:
         """
 
-        product_number = product.info.get('product_number')
+        if product.is_valid:
 
-        query = {
-            'product_number': product_number,
-        }
+            new_record = product.info
 
+            self.__update('products', new_record)
 
-
-    def remove_item(self, remove_query):
+    def remove_item(self, item):
         """ Removes any records that match the given query
 
         :param record:
         :return:
         """
+
+        item_id = item.info.get('item_number')
+
+        self.__remove('items', item_id)
 
     def find_one_item(self, query):
         """
@@ -87,11 +95,15 @@ class Database():
         :return:
         """
 
-        results = self.find_items(query)
+        # results = self.find_items(query)
+        #
+        # if results.__len__() == 1:
+        #
+        #     return results[0]
 
-        if results.__len__() == 1:
+        table = self.db.get('items')
 
-            return results[0]
+        return table.find_one(query)
 
     def find_one_product(self, query):
         """
@@ -121,22 +133,23 @@ class Database():
 
             self.find_one_product(query)
 
-    def find_items(self, query):
+    def find_items(self, query=None):
         """
 
         :param query:
         :return:
         """
 
-        results = []
+        if not query:
+            query = {}
 
         table = self.db.get('items')
 
         items = table.find(query)
 
-        return results
+        return items
 
-    def find_products(self, query):
+    def find_products(self, query=None):
         """
 
         :param query:
@@ -146,6 +159,28 @@ class Database():
         results = []
 
         return results
+
+    def debug(self, function=None, *args):
+        """
+
+        :param function:
+        :param args:
+        :return:
+        """
+
+        functions = {
+            'update': self.__update,
+            'write': self.__write,
+            'remove': self.__remove,
+        }
+
+        if self.debugging:
+            func = functions.get(function)
+            func(*args)
+
+    # def write(self, record_type, record):
+    #
+    #     self.__write(record_type, record)
 
     def __update(self, record_type, new_record):
         """
@@ -157,14 +192,12 @@ class Database():
 
         # new_record = obj.info
 
-        obj = '{record}_number'.format(
-            record=record_type,
-        )
+        record_id_name = self.__select_record_id_name(record_type)
 
-        obj_number = new_record.get(obj)
+        record_id = new_record.get(record_id_name)
 
         query = {
-            obj: obj_number,
+            record_id_name: record_id,
         }
 
         old_record = self.find_one_item(query)
@@ -175,7 +208,7 @@ class Database():
 
         table.update(query, {'$set': changed_fields})
 
-    def __write(self, record, collection):
+    def __write(self, record_type, record):
         """
 
         :param record:
@@ -183,6 +216,38 @@ class Database():
         :return:
         """
 
-        table = self.db.get(collection)
+        table = self.db.get(record_type)
 
         table.insert(record)
+
+    def __remove(self, record_type, record_id):
+        """
+
+        :param record_type:
+        :param query:
+        :return:
+        """
+
+        table = self.db.get(record_type)
+
+        record_id_name = self.__select_record_id_name(record_type)
+
+        query = {
+            record_id_name: record_id,
+        }
+
+        table.remove(query)
+
+    def __select_record_id_name(self, record_type):
+        """
+
+        :param record_type:
+        :return:
+        """
+
+        record_id_name = '{record}_number'.format(
+            record=record_type,
+        )
+
+        return record_id_name
+
