64c1b1c1753ebc2e9d69ff5f7530f7fbea0a97ad
kmjungersen <kmjungersen@gmail.com>
2015-03-17 16:06:32 -0400

    started building inventory models and properly registered app

--- inventory_main/inventory/models.py
+++ inventory_main/inventory/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    barcode_id = models.BigIntegerField()
+    name = models.CharField(max_length=200)
+    product_number = models.BigIntegerField()
+
+    date_first_added = models.DateTimeField('date added')
+    foo = 5
+
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    product = models.ForeignKey(Product)
+
+    item_number = models.BigIntegerField()
+
+    expiration_date = models.DateTimeField()
+    cost = models.DecimalField(decimal_places='2', max_digits=8)
+
+    def __str__(self):
+
+        return self.item_number
--- inventory_main/inventory_main/settings.py
+++ inventory_main/inventory_main/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory_main',
+    'inventory',
 )
 
 MIDDLEWARE_CLASSES = (
