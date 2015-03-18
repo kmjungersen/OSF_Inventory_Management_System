ceb8337c4d80d5f8150c0c21cf2571e9d469ea7b
kmjungersen <kmjungersen@gmail.com>
2015-03-18 12:48:32 -0400

    Modified some features of the admin page

--- inventory_main/inventory/admin.py
+++ inventory_main/inventory/admin.py
@@ -12,14 +12,29 @@ class ProductAdmin(admin.ModelAdmin):
 
     fieldsets = [
         (None, {'fields': ['name']}),
+        ('Product details', {
+            'fields': ['barcode_id', 'product_number']
+        }),
+
         ('Date Information', {
             'fields': ['date_first_added'],
-            'classes': ['collapse']
+            # 'classes': ['collapse']
         })
     ]
+
     inlines = [ItemsInLine]
+    search_fields = ['name', 'barcode_id', 'product_number']
+
+
+
+class ItemAdmin(admin.ModelAdmin):
+
+    list_display = ('item_number', 'expiration_date')
+
+
+
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
 
 admin.AdminSite.site_header = 'INVENTORY!!!!!  YEAH!!'
