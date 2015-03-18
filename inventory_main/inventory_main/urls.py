2233ca9db222197def4b77c1059ccbadfcb89971
kmjungersen <kmjungersen@gmail.com>
2015-03-18 12:50:25 -0400

    modified URLS and settings

--- inventory_main/inventory_main/urls.py
+++ inventory_main/inventory_main/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'inventory_main.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^products/', include('inventory.urls')),
 )
