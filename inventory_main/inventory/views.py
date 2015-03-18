c5a16fa1e050dca15080e22c83bfbdf1c53624b3
kmjungersen <kmjungersen@gmail.com>
2015-03-18 12:50:41 -0400

    created the first 2 views

--- inventory_main/inventory/views.py
+++ inventory_main/inventory/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from inventory.models import Product
 
 # Create your views here.
+
+def index(request):
+    latest_product_list = Product.objects.all()[:5]
+
+    return render(request, 'inventory/products.html', {'products': latest_product_list})
+
+def add_product(request, question_id):
+
+    p = Product(name=request.POST['name'])
+
+    p.save()
+
