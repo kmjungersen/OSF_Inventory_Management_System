70b9d938845f2c7373747b1926d1da406f55ae8c
kmjungersen <kmjungersen@gmail.com>
2015-03-18 23:22:46 -0400

    made significant change to views, including the architecting of the main form submission pages, rendering, etc.

--- inventory_main/inventory/views.py
+++ inventory_main/inventory/views.py
@@ -1,17 +1,103 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from datetime import datetime
 
-from inventory.models import Product
+from inventory.models import Product, Item
+
+
+import ipdb
 
 # Create your views here.
 
 def index(request):
-    latest_product_list = Product.objects.all()[:5]
+    latest_product_list = Product.objects.all()
 
     return render(request, 'inventory/products.html', {'products': latest_product_list})
 
-def add_product(request, question_id):
 
-    p = Product(name=request.POST['name'])
+def add_product_form(request):
+
+    return render(request, 'inventory/add_product.html')
+
+
+def add_product(request):
+
+    p = Product(
+        name=request.POST['name'],
+        barcode_id=request.POST['barcode_id']
+    )
 
     p.save()
 
+    return redirect('index')
+
+
+def lookup_product(request):
+
+    product_found = False
+
+    barcode_id = request.POST['barcode_id']
+
+    try:
+        product = Product.objects.get(barcode_id__exact=barcode_id)
+
+        return render(request, 'inventory/add_item.html', {
+            'first_pass': False,
+            'product_found': True,
+            'barcode_id': product.barcode_id,
+            'product_name': product.name,
+        })
+
+    except Exception as error_message:
+
+        return render(request, 'inventory/add_item.html', {
+            'first_pass': False,
+            'product_found': False,
+            'error_message': error_message,
+        })
+
+
+
+
+def add_item_form(request):
+
+    # if request.POST.get('product_found'):
+    #
+    #     print('foooo')
+
+    return render(request, 'inventory/add_item.html', {
+        'first_pass': True,
+    })
+
+
+def add_item(request):
+
+    barcode_id = request.POST.get('barcode_id')
+    lot_number = request.POST.get('lot_number')
+    cost = request.POST.get('cost')
+    expiration_date_str = str(request.POST.get('expiration_date'))
+    # datetime_str = '{date} {time}'.format(
+    #     date=expiration_date_str,
+    #     time=
+    # )
+    #
+    expiration_date = datetime.strptime(expiration_date_str, '%Y-%m-%d')
+    # time = datetime.time()
+    #
+    #
+    # expiration_date = datetime.combine(expiration_date, time)
+
+    # expiration_date = timezone.datetime(expiration_date_str)
+
+    product = Product.objects.get(barcode_id__exact=barcode_id)
+
+    item = Item(
+        product=product,
+        lot_number=lot_number,
+        cost=cost,
+        expiration_date=expiration_date,
+    )
+
+    item.save()
+
+    return redirect('index')
