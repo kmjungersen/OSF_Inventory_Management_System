47dd05c9e424a24b9a8076589a1a6402aa17d74d
kmjungersen <kmjungersen@gmail.com>
2015-03-18 23:19:32 -0400

    added severalattributes to models and fixed certain ones too

--- inventory_main/inventory/models.py
+++ inventory_main/inventory/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.timezone import datetime, timedelta
+from inventory.local import DEFAULT_EXPIRATION
 
 # Create your models here.
 
@@ -6,11 +8,16 @@ from django.db import models
 class Product(models.Model):
     barcode_id = models.BigIntegerField()
     name = models.CharField(max_length=200)
-    product_number = models.BigIntegerField()
+    description = models.TextField(null=True)
+    notes = models.TextField(null=True)
 
-    date_first_added = models.DateTimeField('date added')
+    #TODO: fix this
+    product_number = models.BigIntegerField(default=0)
+
+    now = datetime.now()
+
+    date_first_added = models.DateTimeField('date added', default=now)
     foo = 5
-
 
     def __str__(self):
         return self.name
@@ -21,20 +28,27 @@ class Product(models.Model):
     def __return_quantity(self):
         return int(self)
 
+    def _compute_average_price(self):
+        pass
+
 
 class Item(models.Model):
     product = models.ForeignKey(Product)
 
-    item_number = models.BigIntegerField()
+    item_number = models.BigIntegerField(default=0)
 
-    expiration_date = models.DateTimeField()
+    default_expiration = timedelta(days=DEFAULT_EXPIRATION) + datetime.now()
+    expiration_date = models.DateField(default=default_expiration)
+
+    lot_number = models.BigIntegerField(null=True)
+
     cost = models.DecimalField(decimal_places=2, max_digits=8)
 
     def __str__(self):
 
         something = 'item #{number} of {product}'.format(
             product=self.product.name,
-            number=str(self.item_number)
+            number=str(self.lot_number)
         )
 
         return something
