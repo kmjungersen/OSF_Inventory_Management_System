8185b976b2ce4924c12db8e49d6ecf6dbd8b8781
kmjungersen <kmjungersen@gmail.com>
2015-03-18 23:21:56 -0400

    added several new urls for the app

--- inventory_main/inventory/urls.py
+++ inventory_main/inventory/urls.py
@@ -4,5 +4,12 @@ from inventory import views
 #
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>\d+)/add_product/$', views.add_product, name='add_product')
+
+    url(r'add_product_form/$', views.add_product_form),
+    url(r'add_item_form/$', views.add_item_form),
+
+    url(r'add_product/$', views.add_product, name='add_product'),
+    url(r'add_item/$', views.add_item, name='add_item'),
+
+    url(r'lookup_product/$', views.lookup_product, name='lookup_product')
 )
