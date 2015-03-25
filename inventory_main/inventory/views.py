c7b5cafba16e0042f086a85ad6ba90f83b15564c
kmjungersen <kmjungersen@gmail.com>
2015-03-24 21:48:41 -0400

    Significant additions and changes to views.py

--- inventory_main/inventory/views.py
+++ inventory_main/inventory/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from datetime import datetime
+from inventory.utils import query_upc_database
 
 from inventory.models import Product, Item
 
@@ -12,19 +13,28 @@ import ipdb
 def index(request):
     latest_product_list = Product.objects.all()
 
-    return render(request, 'inventory/products.html', {'products': latest_product_list})
+    return render(request, 'inventory/products.html', {
+        'products': latest_product_list,
+    })
 
 
-def add_product_form(request):
+def add_product_form(request, error_message=None):
 
-    return render(request, 'inventory/add_product.html', {'first_pass': True})
+    return render(request, 'inventory/add_product.html', {
+        'first_pass': True,
+        'error_messsage': error_message,
+    })
+
+def add_product_form_from_item(request):
+
+    return redirect('add_product_form')
 
 
 def add_product(request):
-    ipdb.set_trace()
+    # ipdb.set_trace()
 
     p = Product(
-        name=request.POST['name'],
+        name=request.POST['product_name'],
         barcode_id=request.POST['barcode_id']
     )
 
@@ -37,29 +47,52 @@ def lookup_product(request, form_origin):
 
     barcode_id = request.POST['barcode_id']
 
+    if barcode_id == '':
+
+        return redirect('add_product_form')
+
     template = 'inventory/add_{origin}.html'.format(
         origin=form_origin,
     )
 
+    product_found = False
+    product_name = ''
+    description = ''
+    # error_message = ''
+
     try:
         product = Product.objects.get(barcode_id__exact=barcode_id)
 
-        return render(request, template, {
-            'first_pass': False,
-            'product_found': True,
-            'barcode_id': product.barcode_id,
-            'product_name': product.name,
-            'description': product.description,
-        })
+        product_found = True
+        product_name = product.name
+        description = product.description
 
-    except Exception as error_message:
+    except Exception:
 
-        return render(request, template, {
-            'first_pass': False,
-            'product_found': False,
-            'barcode_id': barcode_id,
-            'error_message': error_message,
-        })
+        if form_origin == 'product':
+
+            data = query_upc_database(barcode_id)
+
+            if data:
+
+                # ipdb.set_trace()
+
+                # TODO return a selection of possible items and an "is this your item?" dialogue
+                # TODO fix pyscan to be better
+
+                # product_found = True
+
+                product_name = data[0].get('productname')
+                # description = data[0].get('description')
+
+    return render(request, template, {
+        'first_pass': False,
+        'product_found': product_found,
+        'barcode_id': barcode_id,
+        'product_name': product_name,
+        'description': description,
+        # 'error_message': error_message,
+    })
 
 
 def add_item_form(request):
@@ -79,18 +112,8 @@ def add_item(request):
     lot_number = request.POST.get('lot_number')
     cost = request.POST.get('cost')
     expiration_date_str = str(request.POST.get('expiration_date'))
-    # datetime_str = '{date} {time}'.format(
-    #     date=expiration_date_str,
-    #     time=
-    # )
-    #
-    expiration_date = datetime.strptime(expiration_date_str, '%Y-%m-%d')
-    # time = datetime.time()
-    #
-    #
-    # expiration_date = datetime.combine(expiration_date, time)
 
-    # expiration_date = timezone.datetime(expiration_date_str)
+    expiration_date = datetime.strptime(expiration_date_str, '%Y-%m-%d')
 
     product = Product.objects.get(barcode_id__exact=barcode_id)
 
@@ -104,3 +127,19 @@ def add_item(request):
     item.save()
 
     return redirect('index')
+
+
+def home(request):
+
+    return redirect('index')
+
+
+def view_product(request, barcode_id):
+
+    product = Product.objects.get(barcode_id__exact=barcode_id)
+
+    return render(request, 'inventory/view_product.html', {
+        'product': product,
+    })
+
+
