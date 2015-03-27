6f4a195764105451a7bf40b86bf8e8b6de7553f1
kmjungersen <kmjungersen@gmail.com>
2015-03-27 01:40:18 -0400

    added URL's for viewing items, checking items out, and viewing all checked out items

--- inventory_main/inventory/urls.py
+++ inventory_main/inventory/urls.py
@@ -6,9 +6,14 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
 
     url(r'add_product_form/$', views.add_product_form, name='add_product_form'),
-    url(r'add_product_form/P?<error_message>/$', views.add_product_form, name='add_product_form_error'),
-
     url(r'add_item_form/$', views.add_item_form),
+    url(r'view_checked_out_items/$', views.view_checked_out_items, name='checked_out_items'),
+
+    url(r'view_item/(?P<barcode_id>\w+)/(?P<item_id>\w+)/$', views.view_item, name='view_item'),
+    url(r'view_product/(?P<barcode_id>\w+)/(?P<item_id>\w+)/$', views.view_item, name='view_item'),
+
+    url(r'(?P<barcode_id>\w+)/(?P<item_id>\w+)/checkout_form/$', views.checkout_form, name='checkout_form'),
+    url(r'(?P<barcode_id>\w+)/(?P<item_id>\w+)/checkout_form/checkout/$', views.checkout, name='checkout'),
 
     url(r'add_product/$', views.add_product, name='add_product'),
     url(r'add_item/$', views.add_item, name='add_item'),
@@ -18,5 +23,7 @@ urlpatterns = patterns('',
     url(r'home/$', views.home, name='home'),
 
     url(r'view_product/(?P<barcode_id>\w+)/$', views.view_product, name='view_product'),
-    url(r'add_product_form/from_item/$', views.add_product_form_from_item)
+    url(r'add_product_form/from_item/$', views.add_product_form_from_item),
+
+
 )
