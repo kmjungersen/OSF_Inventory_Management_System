663f21087910b8b4c42cd27702fd2008bbb0275c
kmjungersen <kmjungersen@gmail.com>
2015-03-31 20:56:04 -0400

    added some experimental JS functions to handle a redirect, but went a different route

--- inventory_main/inventory/static/inventory/elements.js
+++ inventory_main/inventory/static/inventory/elements.js
@@ -5,3 +5,25 @@
 
 var sh = document.registerElement('sh');
 document.body.appendChild(new sh);
+//
+//
+//function addProduct(element) {
+//    $(element).attr('href', function() {
+//
+//        url = this.href() + retrieveBarcode();
+//        print(this.href());
+//
+//        return url
+//    });
+//}
+//
+//function retrieveBarcode() {
+//    $("#barcode_id")
+//      .keyup(function() {
+//
+//        var value = $( this ).val();
+//
+//        return value;
+//      })
+//      .keyup();
+//}
