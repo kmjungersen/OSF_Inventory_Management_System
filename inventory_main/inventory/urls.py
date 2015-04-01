25dbdbb0972e458a9a096faa3278eac442979b2c
kmjungersen <kmjungersen@gmail.com>
2015-03-31 20:54:07 -0400

    significant changes to URLS, particularly with respect to adding products and items

--- inventory_main/inventory/urls.py
+++ inventory_main/inventory/urls.py
@@ -5,25 +5,34 @@ from inventory import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
 
-    url(r'add_product_form/$', views.add_product_form, name='add_product_form'),
+    # url(r'add_product_form/$', views.add_product_form, name='add_product_form'),
     url(r'add_item_form/$', views.add_item_form),
 
     url(r'view_product/(?P<barcode_id>\w+)/$', views.view_product, name='view_product'),
+    url(r'view_product/(?P<barcode_id>\w+)/add_item/$', views.add_item_form),
+    url(r'view_product/(?P<barcode_id>\w+)/(?P<item_id>\w+)/$', views.view_item),
+
+
     url(r'view_item/(?P<barcode_id>\w+)/(?P<item_id>\w+)/$', views.view_item, name='view_item'),
     url(r'view_checked_out_items/$', views.view_checked_out_items, name='checked_out_items'),
 
     url(r'view_item/(?P<barcode_id>\w+)/(?P<item_id>\w+)/(?P<action>\w+)/$', views.checkout_form, name='checkout_form'),
     url(r'view_item/(?P<barcode_id>\w+)/(?P<item_id>\w+)/(?P<action>\w+)/go/$', views.checkout, name='checkout'),
 
-    url(r'add_product/$', views.add_product, name='add_product'),
-    url(r'add_item/$', views.add_item, name='add_item'),
+    url(r'add_product/$', views.add_product_form, name='add_product_form'),
+    url(r'add_product/lookup/$', views.add_product_form, name='add_product_lookup'),
+    url(r'add_product/(?P<barcode_id>\w+)/$', views.add_product_form, name='add_product'),
 
-    url(r'(?P<form_origin>\w+)/lookup_product/$', views.lookup_product, name='lookup_product'),
+    url(r'add_item/$', views.add_item_form, name='add_item_form'),
+    url(r'add_item/lookup/$', views.add_item_form, name='add_item_lookup'),
+    url(r'add_item/(?P<barcode_id>\w+)/$', views.add_item_form, name='add_item'),
+
+    # url(r'(?P<form_origin>\w+)/lookup_product/$', views.lookup_product, name='lookup_product'),
 
     url(r'home/$', views.home, name='home'),
 
 
-    url(r'add_product_form/from_item/$', views.add_product_form_from_item),
+    url(r'add_product_from_item/$', views.add_product_form_from_item),
 
 
 )
